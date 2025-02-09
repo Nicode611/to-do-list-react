520849e7ad414410002852dc26076878c6f04857
SylverZ <127538225+Nicode611@users.noreply.github.com>
2025-02-09 16:27:32 +1100

    added redux persist

--- to-do-list/src/redux/store.js
+++ to-do-list/src/redux/store.js
@@ -1,9 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit'
-import testReducer from "./slices/testSlice"
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import listsReducer from "./slices/listsSlices"
-export default configureStore({
-    reducer : {
-        test : testReducer,
-        lists : listsReducer
-    }
+
+// La config du reducer persistant (KEY)
+const persistConfig = {
+    key : 'root',
+    storage : storage,
+    blacklist : [''],
+    whitelist : ['lists']
+}
+
+// On utilise la methode combinedReducers de redux toolkit pour pouvoir combiner les reducers si besoin (STORAGE)
+const rootReducer = combineReducers({
+    lists : listsReducer
 })
+
+// Definition du reducer persistant, utilisation de la config (KEY) et des combined reducers (STORAGE)
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    //Le middleware sert a eviter l'erreur dans la console
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+})
+
+const persistor = persistStore(store)
+
+
+export { store, persistor }
